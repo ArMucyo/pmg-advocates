1109ba66db4b14c2ca0ea80a6991855c74bb0c07
Arnaud Mucyo <arnaud.mucyo@ist.com>
2025-06-23 16:34:05 +0200

    PMG Advocates demo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: true, // This enables listening on all network interfaces
     port: 5173  // Default Vite port
